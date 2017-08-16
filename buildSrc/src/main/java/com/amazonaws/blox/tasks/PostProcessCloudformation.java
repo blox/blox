1fd9a6458897fe6ac2e08db183fc889f7c3c344c
Jacob Buys <buys@amazon.com>
2017-08-16 16:50:39 -0700

    Add support for deploying multiple lambda functions
    This expands the PostProcessCloudformation task so that we can
    substitute the CodeUri property for multiple lambda functions using the
    same task.
    
    This also cleans up explicit task dependencies, and relies on Gradle's
    automatic task dependencies configured through input/output file
    properties, so that we don't have to add more `dependsOn` properties for
    future lambda functions.

--- buildSrc/src/main/java/com/amazonaws/blox/tasks/PostProcessCloudformation.java
+++ buildSrc/src/main/java/com/amazonaws/blox/tasks/PostProcessCloudformation.java
@@ -24,10 +24,14 @@ import com.fasterxml.jackson.databind.node.TextNode;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import javafx.util.Pair;
 import lombok.Getter;
 import lombok.Setter;
 import org.gradle.api.DefaultTask;
-import org.gradle.api.tasks.Input;
+import org.gradle.api.file.FileCollection;
 import org.gradle.api.tasks.InputFile;
 import org.gradle.api.tasks.OutputFile;
 import org.gradle.api.tasks.TaskAction;
@@ -43,74 +47,96 @@ import org.gradle.api.tasks.TaskAction;
  * we must provide it as a stage variable instead.
  *
  * <p>Instead, SAM supports specifying the Swagger definition inline using the DefinitionBody field.
- * This allows us to use CloudFormation functions and references to derive the name of the Lambda
+ * This allows us to use Cloudformation functions and references to derive the name of the Lambda
  * function to invoke, but brings its own set of limitations: - Swagger YAML definitions almost
- * always have at least one numeric field name (in the responses object), and CloudFormation
+ * always have at least one numeric field name (in the responses object), and Cloudformation
  * incorrectly reads this as an integer instead of a string key.
  *
  * <p>The solution then is to embed the Swagger definition into the SAM Template from YAML source,
  * but to produce the combined template in JSON format to avoid the non-string key issue.
  *
- * <p>In the same template, cloudformation only supports paths relative to the template file or full
+ * <p>In the same template, Cloudformation only supports paths relative to the template file or full
  * absolute paths for the CodeUri property of a Serverless::Function. This task will substitute in
  * the given lambdaZipFile as an absolute path into the CodeUri property.
+ *
+ * <p>TODO The lambda/swagger stuff is starting to feel like separate concerns. Using a Filter model
+ * similar to the swagger task might make things a bit cleaner
  */
-@Getter
-@Setter
 public class PostProcessCloudformation extends DefaultTask {
+
   /** Mapper used to read the source swagger/SAM YAML templates */
   private ObjectMapper inputMapper = new ObjectMapper(new YAMLFactory());
   /** Mapper used to produce combined JSON template */
   private ObjectMapper outputMapper = new ObjectMapper(new JsonFactory());
-
-  /**
-   * The name of the API GW resource in the SAM template into which the swagger template should be
-   * embedded.
-   */
-  @Input private String apiName;
-
-  /**
-   * The name of the Lambda Function resource in the SAM template into which the Lambda zip path
-   * should be embedded.
-   */
-  @Input private String handlerName;
-
-  /** The YAML file to read the swagger definition from. */
-  @InputFile private File swaggerFile;
-
   /** The YAML file to read the SAM template from. */
-  @InputFile private File templateFile;
-
+  @Getter @Setter @InputFile private File templateFile;
   /** The JSON file to write the combined template into. */
-  @OutputFile private File outputTemplateFile;
+  @Getter @Setter @OutputFile private File outputTemplateFile;
 
-  /** The Zip file to substitute into the CodeUri property of the lambda function */
-  @InputFile private File lambdaZipFile;
+  /**
+   * The lambda functions that should have their CodeUri property updated in the Cloudformation
+   * template.
+   */
+  private Map<String, File> lambdaFunctions = new HashMap<>();
+
+  /**
+   * The API Gateway APIs that should have their DefinitionBody updated in the Cloudformation
+   * template.
+   */
+  private Map<String, File> apis = new HashMap<>();
+
+  /**
+   * Register a lambda function that should have its CodeUri property updated in the Cloudformation
+   * template
+   *
+   * @param ref the CloudFormation Ref of the function to update
+   * @param zip the files containing the function's deployment package
+   */
+  public void lambdaFunction(String ref, FileCollection zip) {
+    getInputs().files(zip);
+    lambdaFunctions.put(ref, zip.getSingleFile());
+  }
+
+  /**
+   * Register an API Gateway API that should have its DefinitionBody property updated in the
+   * Cloudformation template
+   *
+   * @param ref the Cloudformation Ref of the API to update
+   * @param swagger the file containing the swagger definition of the API
+   */
+  public void api(String ref, FileCollection swagger) {
+    getInputs().files(swagger);
+    apis.put(ref, swagger.getSingleFile());
+  }
 
   @TaskAction
   public void embedSwagger() throws IOException {
     JsonNode template = inputMapper.readTree(templateFile);
 
     JsonNode resources = template.get("Resources");
-    embedSwaggerBody(resources);
-    setCodeUri(resources);
+    addApiDefinitionBody(resources);
+    addLambdaCodeURI(resources);
 
     ObjectWriter writer = outputMapper.writer(new DefaultPrettyPrinter());
     outputTemplateFile.getParentFile().mkdirs();
     writer.writeValue(outputTemplateFile, template);
   }
 
-  private void setCodeUri(JsonNode resources) {
-    TextNode codeUri = new TextNode(lambdaZipFile.getAbsolutePath());
+  private void addLambdaCodeURI(JsonNode resources) {
+    for (Entry<String, File> function : lambdaFunctions.entrySet()) {
+      TextNode codeUri = new TextNode(function.getValue().getAbsolutePath());
 
-    ObjectNode apiProperties = (ObjectNode) resources.get(handlerName).get("Properties");
-    apiProperties.set("CodeUri", codeUri);
+      ObjectNode apiProperties = (ObjectNode) resources.get(function.getKey()).get("Properties");
+      apiProperties.set("CodeUri", codeUri);
+    }
   }
 
-  private void embedSwaggerBody(JsonNode resources) throws IOException {
-    JsonNode swagger = inputMapper.readTree(swaggerFile);
+  private void addApiDefinitionBody(JsonNode resources) throws IOException {
+    for (Entry<String, File> api : apis.entrySet()) {
+      JsonNode swagger = inputMapper.readTree(api.getValue());
 
-    ObjectNode properties = (ObjectNode) resources.get(apiName).get("Properties");
-    properties.set("DefinitionBody", swagger);
+      ObjectNode properties = (ObjectNode) resources.get(api.getKey()).get("Properties");
+      properties.set("DefinitionBody", swagger);
+    }
   }
 }
